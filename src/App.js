4ff0d65e1f16f02ff8a6dd29d73b60947e73d006
xodlf8044 <xodlf8044@gmail.com>
2020-11-24 18:02:09 +0900

    React 1-14 (Create Remove)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React , {useRef} from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import Counter from './Counter';
-import InputSample from './InputSample';
+import React, { useState,useRef } from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 import './App.css';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor : 'black',
-    color: 'aqua',
-    fontsize: 24,
-    padding: '1rem'
-  };
-  const users = [
+  // 상태값 선언
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  // 비구조화 할당
+  const { username, email }= inputs;
+  const onChange = e =>{
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+  const [users,setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -30,39 +34,41 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
+
   const nextId = useRef(4);
-  // ref로 특정 값을 선언하면 .current가 그값이 된다.
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    // setUsers(users.concat(user));
+    // concat으로도 배열 복사가능
+    setUsers([...users, user]);
+
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current+=1;
+  };
+
+  const onRemove = id =>{
+    setUsers(users.filter(user=>user.id!==id));
   };
 
 
   return (
-    // 다른 컴포넌트를 묶는 컴포넌트 style을 해당 컴포넌트 내부에서 설정시
-    // 상위 컴포넌트의 props에 children을 넣어주고 내부 컴포넌트 안에 children을 넣어준다.
-    <Wrapper>
-    {/* 주석*/}
-      {/* name을 정해줌 */}
-      <Hello name="react" color="red" />
-      {/* isSpecial이라는 props 설정 true값은 js라서 {}로 묶어주기*/}
-      {/* isSpecial={true} == isSpecial */}
-      <Hello name="isSpecial" color="blue" isSpecial />
-      {/* name을 정해주지 않아서 default로 표시됨 */}
-      <Hello color="pink"/>
-      {/*
-      App 내부에 설정해준 style과 name으로 설정됨
-      div 태그에서 바로 css를 설정해주고 싶은 경우에는 style={sytle}
-      .css에서 css를 설정해주고 싶은 경우에는 className(camelCase 사용)으로 div class 설정
-      */}
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-      {/* Counter 사용하는 방법 */}
-      <Counter/>
-      {/* Input 사용하는 방법 */}
-      <InputSample/>
-      <UserList users={users}/>
-    </Wrapper>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove}/>
+    </>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
-function User({user}){
+function User({user, onRemove}){
     return(
         <div>
             <b>{user.username}</b> <span>({user.email})</span>
+            <button onClick={()=>onRemove(user.id)}>삭제</button>
         </div>
     )
 }
 // map 함수 사용방법
 // app에서 users를 받아와서 users 배열을 돌면서 각각의 user는 User 컴포넌트의 user로 들어간다.
 // map 함수를 쓸 때는 필수로 key도 함께 설정해주어야 한다.
-function UserList({users}){
+function UserList({ users, onRemove }){
     return(
         <div>
             {users.map(user=>(
-                <User user={user} key={user.id}/>
+                <User user={user} key={user.id} onRemove={onRemove}/>
             ))}
         </div>
     );
