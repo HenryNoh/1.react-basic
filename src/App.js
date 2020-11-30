c033c9833b88033005b41de44e82de03a2717058
xodlf8044 <xodlf8044@gmail.com>
2020-11-30 14:57:17 +0900

    React 1-19(Before Using Reducer)

--- src/App.js
+++ src/App.js
@@ -1,115 +1,10 @@
-// import React, { useState, useRef, useMemo } from 'react';
-// import CreateUser from './CreateUser';
-// import UserList from './UserList';
-// import './App.css';
-
-// function countActiveUsers(users){
-//   console.log('활성 사용자를 세는 중');
-//   return users.filter(user=>user.active).length;
-// }
-
-// function App() {
-//   // 상태값 선언
-//   const [inputs, setInputs] = useState({
-//     username: '',
-//     email: ''
-//   });
-//   // 비구조화 할당
-//   const { username, email }= inputs;
-//   const onChange = e =>{
-//     const { name, value } = e.target;
-//     setInputs({
-//       ...inputs,
-//       [name]: value
-//     });
-//   }
-
-//   // useState를 사용할 배열 생성
-//   const [users,setUsers] = useState([
-//     {
-//       id: 1,
-//       username: 'velopert',
-//       email: 'public.velopert@gmail.com',
-//       active: true
-//     },
-//     {
-//       id: 2,
-//       username: 'tester',
-//       email: 'tester@example.com',
-//       active: false
-//     },
-//     {
-//       id: 3,
-//       username: 'liz',
-//       email: 'liz@example.com',
-//       active: false
-//     }
-//   ]);
-
-//   const nextId = useRef(4);
-//   // onCreate 함수 새거 만들때
-//   const onCreate = () => {
-//     const user = {
-//       // current는 usRef를 쓴 nextId의 현재값 받아올 때 사용
-//       // 새로만들 때 id 계속 수정되니까 현재값으로 받아옴
-//       id: nextId.current,
-//       username,
-//       email
-//     };
-//     // setUsers(users.concat(user));
-//     // concat으로도 배열 복사가능
-//     setUsers([...users, user]);
-
-//     setInputs({
-//       username: '',
-//       email: ''
-//     });
-//     nextId.current+=1;
-//   };
-
-//   const onRemove = id =>{
-//     // user.id가 onRemove를 누른 id와 일치하지 않는 원소들만 골라서 user를 새로 만듬
-//     // 그말은 즉 user.id가 같은것만 빼고 새로 만듬
-//     setUsers(users.filter(user=>user.id!==id));
-//   };
-  
-  
-//   const onToggle = id =>{
-//     setUsers(
-//       users.map(user =>
-//         // onToggle를 눌렀을 때 user.id가 id와 같으면
-//         // {...user(user를 불러와서), active(속성을): !user.active(false로 수정)}를 수행
-//         // 아니면 그대로 user 사용
-//         user.id === id ? { ...user, active: !user.active } : user
-//       )
-//     );
-//   };
-//   const count = useMemo(() => countActiveUsers(users), [users]);
-
-//   return (
-//     <>
-//       <CreateUser
-//         username={username}
-//         email={email}
-//         onChange={onChange}
-//         onCreate={onCreate}
-//       />
-//       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
-//       <div>활성사용자 수 : {count}</div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-import React, { useRef, useState, useMemo } from 'react';
-import UserList from './UserList';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 import CreateUser from './CreateUser';
+import UserList from './UserList';
 
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
+function countActiveUsers(users){
+  console.log('활성 사용자를 세는 중');
+  return users.filter(user=>user.active).length;
 }
 
 function App() {
@@ -117,15 +12,21 @@ function App() {
     username: '',
     email: ''
   });
-  const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
-  const [users, setUsers] = useState([
+
+  const { username, email }= inputs;
+
+  const onChange = useCallback(
+    e => {
+      console.log("변화됨");
+      const { name, value } = e.target;
+      setInputs(inputs=>({
+        ...inputs,
+        [name]: value
+      }));
+    },[]
+  );
+
+  const [users,setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -147,34 +48,48 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users.concat(user));
+  const onCreate = useCallback(
+    () => {
+      console.log("생성됨");
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      };
+      setUsers(users => users.concat(user));
 
-    setInputs({
-      username: '',
-      email: ''
-    });
-    nextId.current += 1;
-  };
+      setInputs({
+        username: '',
+        email: ''
+      });
+      nextId.current+=1;
+    },[username,email]
+  );
 
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  };
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
-  const count = useMemo(() => countActiveUsers(users), [users]);
+  const onRemove = useCallback(
+    id =>{
+      console.log("제거됨");
+      setUsers(users => users.filter(user=>user.id!==id));
+    },
+    []
+  );
+  
+  const onToggle = useCallback(
+    id =>{
+      console.log("토글됨");
+      setUsers(users =>
+        users.map(user =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },[]
+  );
+
+  const count = useMemo(
+    () =>
+     countActiveUsers(users), [users]
+  );
+
   return (
     <>
       <CreateUser
@@ -183,7 +98,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
       <div>활성사용자 수 : {count}</div>
     </>
   );
--- src/UserList.js
+++ src/UserList.js
@@ -1,40 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-function User({user, onRemove, onToggle}){
-    useEffect(() => {
-        console.log(user);
-    });
-    return(
-        <div>
-            <b 
-                style={{
-                    cursor: 'pointer',
-                    color: user.active ? 'green' : 'black'
-                }}
-                onClick={()=>onToggle(user.id)}
-            >{user.username}
-            </b>
-            <span>({user.email})</span>
-            <button onClick={()=>onRemove(user.id)}>삭제</button>
-        </div>
-    )
-}
-// map 함수 사용방법
-// app에서 users를 받아와서 users 배열을 돌면서 각각의 user는 User 컴포넌트의 user로 들어간다.
-// map 함수를 쓸 때는 필수로 key도 함께 설정해주어야 한다.
-function UserList({ users, onRemove, onToggle }){
-    return(
-        <div>
-            {users.map(user=>(
-                <User
-                    user={user}
-                    key={user.id}
-                    onRemove={onRemove}
-                    onToggle={onToggle}
-                />
-            ))}
-        </div>
-    );
+const User = React.memo(function User({ user, onRemove, onToggle }) {
+    console.log("User");
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+});
+
+function UserList({ users, onRemove, onToggle }) {
+    console.log("userList");
+  return (
+    <div>
+      {users.map(user => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default UserList;
+export default React.memo(UserList);
